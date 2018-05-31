2dc963410fef3eff0369d108fdd636ae46da3a61
Kim Shrum <kimshrum@Kims-Air.home>
2018-05-31 18:29:37 -0500

    Excited Kittens answered

--- unit_1/w01d04/homework/answers.js
+++ unit_1/w01d04/homework/answers.js
@@ -13,3 +13,23 @@ for(let i = 0; i < 200; i++){
     console.log(i);
   }
 }
+
+// Excited Kitten
+// Write code that logs "Love me, pet me! HSSSSSS!" 20 times.
+//
+// For every even number in your loop, log "...human...why you taking pictures of me?...", "...the catnip made me do it...", or "...why does the red dot always get away..." at random.
+
+let meow = 0;
+const kitten = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+
+for(let i = 0; i < 20; i++){
+  console.log("Love me, pet me! HSSSSSS!");
+  if(i % 2 == 0){
+      meow = Math.round(Math.random() * 10);
+      while(meow > 2){
+        meow -= 3;
+      }
+
+      console.log(kitten[meow]);
+  }
+}
