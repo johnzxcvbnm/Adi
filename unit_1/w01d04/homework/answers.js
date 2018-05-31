5bb390ebf93e8872a7adc04e4f17c29e18145240
Kim Shrum <kimshrum@Kims-Air.home>
2018-05-31 18:48:16 -0500

    Commit - Project Euler Problem 1 answered

--- unit_1/w01d04/homework/answers.js
+++ unit_1/w01d04/homework/answers.js
@@ -97,3 +97,20 @@ const ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"];
 for(const i of ninjaTurtles){
   console.log(i.toUpperCase());
 }
+
+//----------------------------//
+// Multiples of 3 and 5
+// Yes, you might have tackled this earlier, but try it again (don't look back at your other code 👀)
+//
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//
+// Find the sum of all the multiples of 3 or 5 below 1000.
+//
+// 👏 You just solved Project Euler problem 1! 👏
+let sum = 0;
+for(let i = 0; i < 1000; i++){
+  if(i % 3 == 0 || i % 5 == 0){
+    sum += i;
+  }
+}
+console.log("Sum is: " + sum);
