8c56a61d1115aa7dd979195fc9b65aa7af4afe09
Kim Shrum <kimshrum@Kims-Air.home>
2018-05-31 19:12:58 -0500

    Answered methods revisited

--- unit_1/w01d04/homework/answers.js
+++ unit_1/w01d04/homework/answers.js
@@ -114,3 +114,70 @@ for(let i = 0; i < 1000; i++){
   }
 }
 console.log("Sum is: " + sum);
+
+//----------------------------//
+// Methods, Revisited
+// In class, we made an array of our favorite movies:
+//
+const favMovies = ["Black Panther", "V for Vendetta" , "Shawshank Redeption" , "Titanic" , "2001 Space Odyssey" , "Jurassic Park" , "Star Wars" , "Anastasia" , "Harry Potter" , "Star Trek" , "Kill Bill" , "X-Men", "Pulp Fiction", "Interstellar" , "Godfather Part 2"];
+
+// Console log: the index of Anastasia
+console.log("Index of Anastasia is " + favMovies.indexOf("Anastasia"));
+
+// Do the following and console.log the final results (I have included some thought questions, you don't have to write out an answer for those marked as such):
+//
+// use the .sort method Thought question: what did this do to the array? Did it permanently alter it?
+console.log("Movies Sorted")
+// console.log(favMovies.sort());
+// console.log(favMovies);
+
+// Use the method pop
+console.log("Used 'pop' method");
+favMovies.pop();
+console.log(favMovies);
+
+// push "Guardians of the Galaxy"
+console.log("Used 'push' method");
+favMovies.push("Guardians of the Galaxy");
+console.log(favMovies);
+
+// Reverse the array
+console.log("Used 'reverse' method");
+favMovies.reverse();
+console.log(favMovies);
+
+// Use the shift method
+console.log("Used 'shift' method");
+favMovies.shift();
+console.log(favMovies);
+
+// unshift "Hackers"
+console.log("Used 'unshift' method");
+favMovies.unshift("Hackers");
+console.log(favMovies);
+
+// splice "Titanic" and add "Avatar" (try finding the index of "Titanic", instead of counting it yourself) Thought question: did this permanently alter our array?
+console.log("Used 'splice' method");
+favMovies.splice(favMovies.indexOf("Titanic"), 1, "Avatar");
+console.log(favMovies);
+
+// slice the last half of the array (challenge yourself and try to programatically determine the middle of the array rather than counting it and hard coding it) - Thought question: did this permanently alter our array?
+console.log("Used 'slice' method");
+console.log(favMovies.slice((favMovies.length / 2), favMovies.length));
+// console.log(favMovies);
+
+// store the value of your slice in a variable, console.log it - Thought question: what is going on here?
+console.log("Used 'slice' method again");
+const newFavMovies = favMovies.slice((favMovies.length / 2), favMovies.length);
+console.log(newFavMovies);
+
+// console.log your final results
+console.log(favMovies);
+
+// Console.log the index of "Godfather Part 2" -We removed it from the array, what value do we get when we look for the index of something that is not in the array?
+console.log("Used 'indexOf' method");
+console.log(favMovies.indexOf("Godfather Part 2"));
+//Output should be a '-1'
+
+// Thought question: that we declared the variable favMovies with const, and yet, we were allowed to change the array. Weird? Should we have used let?
+//When we declare the array favMovies we are actually creating a pointer which simply points to where the variable is in memory, so when we change something in the array we're not changing where it is located in memory.
