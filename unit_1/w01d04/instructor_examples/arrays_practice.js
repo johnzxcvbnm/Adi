2a56030c2476509f85e0304e8139bade44a3ee31
Madeline <madeline@done-digital.com>
2018-06-03 16:09:15 -0700

    combining

---
+++ unit_1/w01d04/instructor_examples/arrays_practice.js
@@ -0,0 +1,11 @@
+const arr = [];
+
+const pairs = [["Snoopy", "Linus"], ["Peppermint Patty", "Woodstock"]];
+
+pairs[0][0] = "Charlie";
+
+console.log(pairs[0][0]);
+
+const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+                    ["Lucinda", "Jacc", "Neff", "Snoop"],
+                    ["Petunia", "Baked Goods", "Waldo"]];
