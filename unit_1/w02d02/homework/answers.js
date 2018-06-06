5f5070ddb7ae3154542583e35aeea4b504d2acaf
Kim Shrum <kimshrum@Kims-Air.home>
2018-06-05 19:36:31 -0500

    Created some data structures and accessed some data

--- unit_1/w02d02/homework/answers.js
+++ unit_1/w02d02/homework/answers.js
@@ -49,3 +49,22 @@
 //   { name: "John", location: "Texas", favShows: ["Rick and Morty", "Family Guy"] },
 //   { name: "Keith", location: "I dont' know", favShows: ["Law and Order", "Law and Order: Special Victims Unit"] },
 //   ...]
+
+
+//---Take it Easy---//
+// Make an array that holds all of the colors of the rainbow.
+const rainbow = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"];
+
+// Write code that will access "blue" from the rainbow array.
+console.log(rainbow[rainbow.indexOf("Blue")]);
+
+// Make an object that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype.
+const johnKusching = {
+  favFood: "Cheese Cake",
+  hobby: "Video Games",
+  town: "Plano",
+  favData: "Arrays"
+};
+
+// Write code that will access your hobby from the object that you just created.
+console.log(johnKusching.hobby);
