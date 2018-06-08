f374acfb2f3f3f0fe1d40e17862bd8df20ad0717
Kim Shrum <kimshrum@Kims-Air.home>
2018-06-07 20:36:09 -0500

    Dougie and Pizza Rat fought

--- unit_1/w02d04/homework/donut_adventure/adventure.js
+++ unit_1/w02d04/homework/donut_adventure/adventure.js
@@ -22,7 +22,7 @@ class Hero {
   constructor(name){
     this.name = name;
     this.health = 100;
-    this.weapons = { sprinkleSpray: 5, sugarShock: 5 };
+    this.weapons = { sprinkleSpray: 5, sugarShock: 10 };
     this.catchPhrases = ["I\'m fresher than day old pizza!", "You can\'t count my calories!"];
   }
 
@@ -33,12 +33,20 @@ class Hero {
 
   //Logs current heath
   announceHealth() {
-    console.log("I'm currently at " + this.health + " HP");
+    console.log("I " + this.name + " am currently at " + this.health + " HP.");
   }
 
   //Logs ready to rumble
-  fight() {
+  fight(foe) {
     console.log("I\'m ready to rumble!");
+    let weaponChoice = Math.floor(Math.random() * 2);
+    if(weaponChoice == 0){
+      foe.health -= 5;
+      console.log(foe.name + " was hit by Sprinkle Spray!  His health is now at " + foe.health + ".");
+    } else {
+      foe.health -= 10;
+      console.log(foe.name + " was hit by Sugar Shock!  His health is now at " + foe.health + ".");
+    }
   }
 }
 
@@ -84,17 +92,56 @@ class Enemy {
 
   //Logs current heath
   announceHealth() {
-    console.log("I'm currently at " + this.health + " HP");
+    console.log("I, " + this.name + " am currently at " + this.health + " HP.");
   }
 
   //Logs ready to rumble
-  fight() {
+  fight(foe) {
     console.log("I\'m gonna flatten you like a slice of pepperoni!");
+    let weaponChoice = Math.floor(Math.random() * 2);
+    if(weaponChoice == 0){
+      foe.health -= 5;
+      console.log(foe.name + " got hit by Pepperoni Stars!  His health is now at " + foe.health + ".");
+    } else {
+      foe.health -= 10;
+      console.log(foe.name + " was hit by Cheese Grease!  His health is now at " + foe.health + ".");
+    }
   }
 }
 
 const remy = new Enemy("Remy");
-console.log(remy);
+// console.log(remy);
+// remy.talkSmack();
+// remy.announceHealth();
+// remy.fight();
+
+
+//----------------------------Walking down the street
+// Now, let's write their story! Dougie is walking down Flat Iron -- but oh no! He runs into Pizza rat!
+//
+// Have Dougie talkSass
+// Have Pizza Rat talkSmack
+// Have Dougie announceHealth
+// Have Pizza Rat announceHealth
+dougie.talkSass();
 remy.talkSmack();
+dougie.announceHealth();
 remy.announceHealth();
-remy.fight();
+
+//----------------------------------Fight!
+// Things have escalated between Dougie and Pizza Rat!
+//
+// Upgrade their fight methods so that it accesses one of their weapons and console log its hitpoints.
+// Keep upgrading this fight method to accept an argument called enemy so that when you call on the fight method you can pass in the entire Dougie or Pizza Rat object as the parameter. (i.e. dougie.fight(pizzaRat))
+// Now that we are able to pass in Dougie or Pizza Rat as an object, we can make it so that our fight method subtracts from their health.
+// Using the hitpoints from the weapon they're using, subtract that amount from the enemy's health (i.e. If Dougie fights Pizza Rat using sprinkleSpray, subtract sprinkleSpray's hitpoint value (5) from Pizza Rat's health)
+// Console log the enemy name and their new health value (i.e. 'Dougie got hit by pepperoniStars! His health is now at 95!')
+// Now, they can fight!
+//
+// Have Pizza Rat fight Dougie
+// Have Dougie fight Pizza Rat
+// Have Pizza Rat and Dougie both announceHealth to make sure their health has decreased!
+remy.fight(dougie);
+dougie.fight(remy);
+dougie.announceHealth();
+remy.announceHealth();
