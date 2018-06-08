4c47f303b771f89b46e9b3b5ee4456cc7b85f9a4
Kim Shrum <kimshrum@Kims-Air.home>
2018-06-07 19:56:55 -0500

    Created Pizza Rat

--- unit_1/w02d04/homework/donut_adventure/adventure.js
+++ unit_1/w02d04/homework/donut_adventure/adventure.js
@@ -43,7 +43,58 @@ class Hero {
 }
 
 const dougie = new Hero("Dougie");
-console.log(dougie);
-dougie.talkSass();
-dougie.announceHealth();
-dougie.fight();
+// console.log(dougie);
+// dougie.talkSass();
+// dougie.announceHealth();
+// dougie.fight();
+
+
+//------------------Enemy:
+// Now let's create our Enemy class!
+//
+// Attributes:
+// name: set name from parameter in constructor method
+// health: initially 100
+// weapons: use the following object
+// {
+//     pepperoniStars: 5,
+//     cheeseGrease: 10
+// }
+// catchPhrases:
+// ['i\'m youtube famous',
+// 'i\'m more dangerous than an uncovered sewer']
+// Methods:
+// talkSmack: logs one of his catchphrases randomly
+// announceHealth: logs his current health
+// fight: for now, logs i\'m gonna flatten you like a slice of pepperoni!
+// Now, using this Enemy class, create an instance of the enemy Pizza Rat.
+
+class Enemy {
+  constructor(name){
+    this.name = name;
+    this.health = 100;
+    this.weapons = { pepperoniStars: 5, cheeseGrease: 10 };
+    this.catchPhrases = ["I\'m YouTube famous!", "I\'m more dangerous than an uncovered sewer!"];
+  }
+
+  //Randomly logs one of the catch phrases
+  talkSmack() {
+    console.log(this.catchPhrases[Math.floor(Math.random() * this.catchPhrases.length)]);
+  }
+
+  //Logs current heath
+  announceHealth() {
+    console.log("I'm currently at " + this.health + " HP");
+  }
+
+  //Logs ready to rumble
+  fight() {
+    console.log("I\'m gonna flatten you like a slice of pepperoni!");
+  }
+}
+
+const remy = new Enemy("Remy");
+console.log(remy);
+remy.talkSmack();
+remy.announceHealth();
+remy.fight();
