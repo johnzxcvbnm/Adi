ebe458e8b3ca7ace79ef94dbdcabedbbdbd44ecb
Kim Shrum <kimshrum@Kims-Air.home>
2018-06-13 19:32:07 -0500

    Added comments throughout and added a special case

--- unit_1/w03d03/student_labs/todos_starter/app.js
+++ unit_1/w03d03/student_labs/todos_starter/app.js
@@ -1,35 +1,62 @@
+//Function removes current event target from the DOM
+//Is used for when the user clicks the 'REMOVE' button from the 'Things That are Done' list
+const remove = (event) => {
+  $(event.currentTarget).parent().remove();
+}
 
-$( () => {
+//Function removes items from the 'Things to Do' list to the 'Things That are Done' list
+const moveToDone = (event) => {
 
-  const remove = (event) => {
-    $(event.currentTarget).parent().remove();
-  }
+  //Creates a new <p> tag and pulls the text from the event target
+  //Event target is the 'COMPLETED' button that the user has clicked on
+  const $myText = $("<p>").text( $(event.currentTarget).siblings().text() );
 
-  const moveToDone = (event) => {
+  //Removes current event target from the DOM (aka, the 'Things to Do' list)
+  $(event.currentTarget).parent().remove();
 
-    const $myDiv = $("<div>").addClass("done-item");
-    const $myButton = $("<button>").text("REMOVE");
-    $myButton.on("click", remove);
-    let myTest = $(event.currentTarget).parent().text();
+  //Creates a new button 'REMOVE' with the remove event when the user clicks on the button
+  const $myButton = $("<button>").text("REMOVE");
+  $myButton.on("click", remove);
 
-    myTest = myTest.slice(0, myTest.length - 9);
-    $(event.currentTarget).parent().remove();
-    $myDiv.text(myTest);
-    $myDiv.append($myButton);
-    $("#completed").append($myDiv);
-  }
+  //Creates a new div to add to the 'Things That are Done' list
+  //Adds the newly created text and button to the div
+  //Then adds the div to the top of the 'Things that are Done' list
+  const $myDiv = $("<div>").addClass("done-item");
+  $myDiv.append($myText).append($myButton);
+  //I added the ID 'areDone' in the HTML file to add the div right after the h2 tag
+  $("#areDone").after($myDiv);
 
-  const addToDo = () => {
-    const $newItem = $("<div>").addClass("to-do-item");
+  //This last line is if you wanted to add items to the bottom of the list
+  //This adds the new div to the bottom of the 'completed' div
+  // $("#completed").append($myDiv);
+}
+
+//Function pulls input from the user and adds it to the 'Things to Do' list
+const addToDo = () => {
+  //Creates a new <p> tag and adds text from the input
+  const $myText = $("<p>").text( $("#input-box").val() );
+
+  //If the user just clicks the add button or tries to add spaces then nothing is executed
+  if($myText.text().replace(/\s/g, '') != ""){
+    //Creates a new button with the text "COMPLETED"
+    //Adds a listener which, when executed, moves the clicked on task to the 'Things That are Done' list
     const $myButton = $("<button>").text("COMPLETED");
-
-    $newItem.text( $("#input-box").val() );
     $myButton.on("click", moveToDone);
-    $newItem.append($myButton);
-    $("#to-do-list").append( $newItem );
-  }
 
+    //Creates a new div and adds the new <p> and button to it
+    //The new div is then attached to the top of the 'Things to Do' list
+    const $newItem = $("<div>").addClass("to-do-item");
+    $newItem.append($myText).append($myButton);
+    $("#toDo").after($newItem);
+
+    //The las line is if you wanted to add things to the bottom of the list instead
+    //This adds the new div to the bottom of the 'Things to Do' list
+    // $("#to-do-list").append( $newItem );
+  }
+}
+
+//Document Ready Function
+$( () => {
+  //Adds a listener which, when activated, takes the input and adds it to the 'Things to Do' list
   $("#submit").on("click", addToDo );
-    // event.preventDefault();
-    // $(event.currentTarget).trigger("reset");
 }); //End of On Ready Function
