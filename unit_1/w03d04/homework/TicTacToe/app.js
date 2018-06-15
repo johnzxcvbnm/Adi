876decbe4204f517233ece06cb51f6aec21ba078
John Kusching <kimshrum@Kims-Air.home>
2018-06-15 14:30:37 -0500

    Finished adding comments to Tic Tac Toe

--- unit_1/w03d04/homework/TicTacToe/app.js
+++ unit_1/w03d04/homework/TicTacToe/app.js
@@ -120,47 +120,72 @@ const tieCheck = () => {
 }
 
 //Function determines if a space can be played on
+//If the space has no text then it can be played on
 const canPlay = ($target) => {
   return $target.children().text() === "";
 }
 
-
+//Function places an X or O on the clicked square, if it can be played on
+//Then it checks to see if one of the players has won, or if the game is tied
 const playSquare = (event) => {
+  //Pulls the square that was clicked on
   const $myTarget = $(event.currentTarget);
 
+  //If you can play on it, then it places the apropriate letter on the square
   if(canPlay($myTarget)){
     $myTarget.children().text(myTurn[0]);
+    //Check to see if someone won the game
     winCondition();
+    //Check to see if the game is a tie
     tieCheck();
+    //Pushes whoever went to the back of the array, thus rotating whose turn it is
     myTurn.push(myTurn[0])
     myTurn.shift();
   }
 }
 
+//Function generates the field of play based off of the users input
+//It generates (num times num) number of squares to populate the field
+//Each square contains a h2 which holds who played there (or empty string)
+//Along with a handler which allows you to play on that square
 const generateSquares = (num) => {
+  //Reset button resets the field and resets the player array, so X always goes first
   $(".grid").empty();
   myTurn[0] = "X";
   myTurn[1] = "O";
 
-
+  //width of each square so it populates the field correctly with lines in between each box
   let width = Math.floor(90 / num);
 
+  //Special case for a 5x5 and 6x6 so it renders correctly
   if(num == 5 || num == 6){
     width = Math.floor(87 / num);
   }
-  // $("h2").css("line-height", width);
+
+  //Generates num times num (3x3, 4x4, etc) number of squares based off of the users input
   for(let i = 0; i < num * num; i++){
+    //Create a new square
     const $mySquare = $("<div>").addClass("square");
+    //Set the height and width based off of the above calculation
     $mySquare.css("width", width + "%");
     $mySquare.css("height", width + "%");
+    //Add an empty h2 to hold who plays there, and sets the font size for different size fields
     $mySquare.html("<h2></h2>");
     $mySquare.css("font-size", (3 * width) + "px");
+    //Sets an ID up as a cordinate system, so the top row of a 3x3 would be (0-0, 0-1, 0-2)
+    //This allows the system to iterate through the grid to determine if there is a winner
     $mySquare.attr("id", (i%num) + "-" + Math.floor(i/num));
+    //Create a handler for when the user clicks on the square
     $mySquare.on("click", playSquare);
+    //Add the square to the container 'grid'
     $(".grid").append($mySquare);
   }
 }
 
+//Function pulls the input from the input box after the user clicks on the 'RESET' button
+//If the input is valid ( only numbers 3 - 7 will be accepted ) the apropriate grid is then drawn
+//Otherwise nothing happens
+//Input range is in place because drawing more than 7 squares wide breaks out of the container that has the playing squares, thus breaking the game logic
 const pullInput = () => {
   gridNum = parseInt($("#inputBox").val());
   if( (gridNum > 2) && (gridNum < 8) ){
@@ -168,6 +193,8 @@ const pullInput = () => {
   }
 }
 
+//Document Ready Function
+//Adds a listener to the 'RESET' button
 $( () => {
   $("#inputButton").on("click", pullInput);
-});
+});//End of Document Ready Function`
