be78d7d09dfd940a59fc9fc708c2281983b87d84
Matt Huntington <matt.huntington@gmail.com>
2018-06-28 15:41:47 -0400

    edit/update

--- unit_2/w05d04/instructor_examples/fruits/server.js
+++ unit_2/w05d04/instructor_examples/fruits/server.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const app = express();
 const fruits = require('./models/fruits.js');
-
+const methodOverride = require('method-override');
 // app.use((request, response, next)=>{
 //     console.log('a request has been received');
 //     next();
 // })
 
+app.use(methodOverride('_method'));
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:false}));
+
+app.get('/fruits/:indexOfFruitsArray/edit', (request, response)=>{
+    response.render('edit.ejs',{
+        fruit:fruits[request.params.indexOfFruitsArray],
+        index: request.params.indexOfFruitsArray
+    });
+});
+
+app.put('/fruits/:indexOfFruitsArray', (request, response)=>{
+    if(request.body.readyToEat === 'on'){
+        request.body.readyToEat = true;
+    } else {
+        request.body.readyToEat = false;
+    }
+    fruits[request.params.indexOfFruitsArray] = request.body;
+    response.redirect('/fruits');
+})
 
 app.get('/fruits', (request, response)=>{
     response.render('index.ejs', {
@@ -16,6 +34,10 @@ app.get('/fruits', (request, response)=>{
     });
 });
 
+app.delete('/fruits/:indexOfFruitsArray', (request, response)=>{
+    fruits.splice(request.params.indexOfFruitsArray, 1);
+    response.redirect('/fruits');
+});
 // app.post('/products', (req, res)=>{
 //     console.log('create route accessed!');
 //     console.log('Req.body is: ', req.body);
