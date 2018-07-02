8ef58cb7055197c84ef46c39defcabcc32363d19
John Kusching <kimshrum@Kims-Air.home>
2018-07-02 15:22:12 -0500

    Created a DELETE route for the Captains logs

--- unit_2/w06d01/student_labs/captains_log/server.js
+++ unit_2/w06d01/student_labs/captains_log/server.js
@@ -1,5 +1,6 @@
 //Dependencies
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const express = require("express");
 const CapLog = require("./models/logs.js");
 
@@ -9,7 +10,10 @@ const app = express();
 const port = 3000;
 
 //Middleware
+app.use( methodOverride("_method") );
+app.use( express.static("public") );
 app.use( express.urlencoded( {extended: true} ) );
+
 
 //Listen Routes
 app.listen(port, () => {
@@ -29,16 +33,47 @@ app.post("/logs/", (req, res) => {
   });
 });
 
+//Delete Routes
+app.delete("/logs/:id", (req, res) => {
+  CapLog.remove( { _id: req.params.id }, (err, myLog) => {
+    res.redirect("/logs");
+  });
+});
+
 //Get Routes
+app.get("/logs/seed", (req, res) => {
+  const mySeedData = [
+    {
+      title: "Start Mission",
+      entry: "Today we started on a new mission.  To blow up space.  It'll be a very long and difficult mission, but only a captain as crazy as I am can do it.",
+      shipIsBroken: false
+    },
+    {
+      title: "Mars Landing",
+      entry: "We landed on Mars this evening, to refuel and restock.  Unfortunately our pilot was drunk and crashed the ship on landing.  We will be here for a few days while repairs are under way.",
+      shipIsBroken: true
+    },
+    {
+      title: "Good-bye Pluto",
+      entry: "We did it!  We blew up Pluto!  It was our first victory in this long war, but a well deserved one.  Next up, deep space, sector nine.",
+      shipIsBroken: false
+    }
+  ];
+
+  CapLog.create( mySeedData, (err, data) => {
+    res.redirect("/logs");
+  });
+});
+
 app.get("/logs/new", (req, res) => {
   res.render("new.ejs");
 });
 
 app.get("/logs/:index", (req, res) => {
-  CapLog.find( { _id: req.params.index }, (error, myLog) => {
+  CapLog.findById( req.params.index, (error, myLog) => {
     // res.send(myLog);
     res.render("show.ejs", {
-      log: myLog[0]
+      log: myLog
     });
   });
 });
