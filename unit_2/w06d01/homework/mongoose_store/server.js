adc7ec24073597aa07820c19bd10a5df54821361
John Kusching <kimshrum@Kims-Air.home>
2018-07-04 12:56:35 -0500

    Added more Seed Data and some CSS Styling

--- unit_2/w06d01/homework/mongoose_store/server.js
+++ unit_2/w06d01/homework/mongoose_store/server.js
@@ -66,22 +66,40 @@ app.post("/jonk", (req, res) => {
 app.get('/jonk/seed', async (req, res) => {
 
   const newProducts = [
-		{
+    {
+      name: "Bag of Mystery",
+      description: "What's in the bag?  Who knows!  It's a mystery!",
+      img:"https://media.istockphoto.com/photos/garbage-bag-picture-id147689606?k=6&m=147689606&s=612x612&w=0&h=09ai_Y2t1LniLgwZ9Fo1w5h5GK0-XGQ09RFIm_q7DcQ=",
+      price: 350,
+      qty: 5
+    }, {
+      name: "Rusty Nails",
+      description: "Ever hear that saying 'You're nailing it'?  Well this is where it comes from, nails!  So if you want to 'nail it' in all aspects of life, this is a must have!",
+      img:"https://2rdnmg1qbg403gumla1v9i2h-wpengine.netdna-ssl.com/wp-content/uploads/sites/3/2015/06/RustyNails-650x450.jpg",
+      price: 10,
+      qty: 200
+    }, {
+      name: "Slime",
+      description: "Get tomorrows' super food, today!  This blue gelatinous delicacy is harvested weekly as it forms mysteriously on the sewer cover in my alleyway.  Just don't tell the FDA about it.",
+      img:"http://redwork.co/wp-content/uploads/2018/05/clear-slime-with-contact-solution-blue-clear-slime-scented-clear-blue-slime-scented-slime-super-clear-slime-clear-blue-slime-blue-glass-slime-slime-fun-slime-clear-slime-contact-solution.jpg",
+      price: 25,
+      qty: 30
+    }, {
 			name: "Beans",
 			description: "A small pile of beans. Buy more beans for a big pile of beans.",
-	        img: "https://cdn3.bigcommerce.com/s-a6pgxdjc7w/products/1075/images/967/416130__50605.1467418920.1280.1280.jpg?c=2",
+	    img: "https://cdn3.bigcommerce.com/s-a6pgxdjc7w/products/1075/images/967/416130__50605.1467418920.1280.1280.jpg?c=2",
 			price: 5,
 			qty: 99
 		}, {
 			name: "Bones",
 			description: "It's just a bag of bones.",
-    	    img: "http://bluelips.com/prod_images_large/bones1.jpg",
+	    img: "http://bluelips.com/prod_images_large/bones1.jpg",
 			price: 25,
 			qty: 0
 	  }, {
 			name: "Bins",
 			description: "A stack of colorful bins for your beans and bones.",
-    	    img: "http://www.clipartbest.com/cliparts/9cz/rMM/9czrMMBcE.jpeg",
+    	img: "http://www.clipartbest.com/cliparts/9cz/rMM/9czrMMBcE.jpeg",
 			price: 7000,
 			qty: 1
 	  }
