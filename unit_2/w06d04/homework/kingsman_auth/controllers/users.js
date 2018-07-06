67a82d434d3dc4d1fad80cb30675683fdcd5c37e
John Kusching <kimshrum@Kims-Air.home>
2018-07-05 19:36:24 -0500

    Finished Creation Route

--- unit_2/w06d04/homework/kingsman_auth/controllers/users.js
+++ unit_2/w06d04/homework/kingsman_auth/controllers/users.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/users.js");
 
 router.get("/new", (req, res) => {
   res.render("users/new.ejs");
+});
+
+router.post("/", (req, res) => {
+  const myArray = req.body.messages.split(".");
+  req.body.messages = myArray.map( x => x.trim() );
+  User.create(req.body, (err, createdUser) => {
+    res.send(createdUser);
+  });
 });
 
 module.exports = router;
