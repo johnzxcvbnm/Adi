2c908a0277f1dc593b5e3f9e8c54b79fb24446b7
John Kusching <kimshrum@Kims-Air.home>
2018-07-13 20:04:21 -0500

    FAST LANE - DriversController is created

--- unit_3/w07d05/homework/part-1/app.js
+++ unit_3/w07d05/homework/part-1/app.js
@@ -4,5 +4,8 @@ app.controller("CarsController", function() {
   this.myTitle = "My Awesome Cars";
   this.types = ["Honda", "Toyota", "Mazda", "Chevy", "Tesla", "Volvo", "Saab", "Fiat"];
   this.colors = ["mistyrose", "cornflowerblue", "lemonchiffon", "tomato", "azure", "snow", "orchid", "blanchedalmond", "papayawhip" ,"thistle"];
+});
 
+app.controller("DriversController", function() {
+  
 });
