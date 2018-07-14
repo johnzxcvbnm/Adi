08fee2544d0e72304036de21f0e7fad09f7ceaec
John Kusching <kimshrum@Kims-Air.home>
2018-07-13 20:15:00 -0500

    FAST LANE - Driver picture is hidden when isShy is true

--- unit_3/w07d05/homework/part-1/app.js
+++ unit_3/w07d05/homework/part-1/app.js
@@ -9,5 +9,5 @@ app.controller("CarsController", function() {
 app.controller("DriversController", function() {
   this.name = "John Kusching, Lord of the Road";
   this.driverImg = "http://bigcitydriver.com/wp-content/uploads/2013/06/road-rage-cat-driving-300x199.jpg";
-  this.isShy = false;
+  this.isShy = true;
 });
