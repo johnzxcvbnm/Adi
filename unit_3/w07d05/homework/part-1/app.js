26f0930eb0e2027e2cc011deca0546688756773a
John Kusching <kimshrum@Kims-Air.home>
2018-07-13 20:34:06 -0500

    FAST LANE - Stretch Goals

--- unit_3/w07d05/homework/part-1/app.js
+++ unit_3/w07d05/homework/part-1/app.js
@@ -4,10 +4,26 @@ app.controller("CarsController", function() {
   this.myTitle = "My Awesome Cars";
   this.types = ["Honda", "Toyota", "Mazda", "Chevy", "Tesla", "Volvo", "Saab", "Fiat"];
   this.colors = ["mistyrose", "cornflowerblue", "lemonchiffon", "tomato", "azure", "snow", "orchid", "blanchedalmond", "papayawhip" ,"thistle"];
+
+  this.newColor = "";
+  this.newType = "";
+
+  this.addColor = () => {
+    this.colors.push(this.newColor);
+  }
+
+  this.addType = () => {
+    this.types.push(this.newType);
+  }
+
 });
 
 app.controller("DriversController", function() {
   this.name = "John Kusching, Lord of the Road";
   this.driverImg = "http://bigcitydriver.com/wp-content/uploads/2013/06/road-rage-cat-driving-300x199.jpg";
-  this.isShy = true;
+  this.isShy = false;
+
+  this.showHide = () => {
+    this.isShy = !this.isShy;
+  }
 });
