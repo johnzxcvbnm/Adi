03edfb22c427c68eeed73261ede8264698a51ec4
John Kusching <kimshrum@Kims-Air.home>
2018-07-13 19:52:30 -0500

    FAST LANE - types and color are set in the CC

--- unit_3/w07d05/homework/part-1/app.js
+++ unit_3/w07d05/homework/part-1/app.js
@@ -1,5 +1,6 @@
 const app = angular.module("myHomework", []);
 
 app.controller("CarsController", function() {
-
+  const types = ["Honda", "Toyota", "Mazda", "Chevy", "Tesla", "Volvo", "Saab", "Fiat"];
+  const colors = ["mistyrose", "cornflowerblue", "lemonchiffon", "tomato", "azure", "snow", "orchid", "blanchedalmond", "papayawhip" ,"thistle"];
 });
