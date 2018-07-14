4ab73d67e39d166809aaa325ea66c6015aaed917
John Kusching <kimshrum@Kims-Air.home>
2018-07-13 19:59:55 -0500

    FAST LANE - color and type are displaying on the page

--- unit_3/w07d05/homework/part-1/app.js
+++ unit_3/w07d05/homework/part-1/app.js
@@ -1,6 +1,8 @@
 const app = angular.module("myHomework", []);
 
 app.controller("CarsController", function() {
-  const types = ["Honda", "Toyota", "Mazda", "Chevy", "Tesla", "Volvo", "Saab", "Fiat"];
-  const colors = ["mistyrose", "cornflowerblue", "lemonchiffon", "tomato", "azure", "snow", "orchid", "blanchedalmond", "papayawhip" ,"thistle"];
+  this.myTitle = "My Awesome Cars";
+  this.types = ["Honda", "Toyota", "Mazda", "Chevy", "Tesla", "Volvo", "Saab", "Fiat"];
+  this.colors = ["mistyrose", "cornflowerblue", "lemonchiffon", "tomato", "azure", "snow", "orchid", "blanchedalmond", "papayawhip" ,"thistle"];
+
 });
