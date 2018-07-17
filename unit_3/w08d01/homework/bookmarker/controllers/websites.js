454667d8df82c60ae2959e693fcf32c352446939
John Kusching <kimshrum@Kims-Air.home>
2018-07-16 19:06:05 -0500

    Created POST, PUT, and DELETE routes and tested them successfully

--- unit_3/w08d01/homework/bookmarker/controllers/websites.js
+++ unit_3/w08d01/homework/bookmarker/controllers/websites.js
@@ -1,5 +1,32 @@
+//Dependancies
 const express = require("express");
 const Website = require("../models/websites.js");
 const router = express.Router();
+
+//Get Routes
+router.get("/", (req, res) => {
+  res.send("Index");
+});
+
+//Delete Routes
+router.delete("/:id", (req, res) => {
+  Website.findByIdAndRemove(req.params.id, (err, deletedWebsite) => {
+    res.json(deletedWebsite);
+  });
+});
+
+//Put Routes
+router.put("/:id", (req, res) => {
+  Website.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedWebsite) => {
+    res.json(updatedWebsite);
+  });
+});
+
+//Post Routes
+router.post("/", (req, res) => {
+  Website.create(req.body, (err, createdWebsite) => {
+    res.json(createdWebsite);
+  });
+});
 
 module.exports = router;
