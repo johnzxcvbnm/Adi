e3db24bf9c6b7907c7555224232eac0b5f531a7e
Matt Huntington <matt.huntington@gmail.com>
2018-07-17 11:08:07 -0400

    update/delete examples

--- unit_3/w08d02/instructor_examples/CRUD/public/js/app.js
+++ unit_3/w08d02/instructor_examples/CRUD/public/js/app.js
@@ -2,6 +2,7 @@ const app = angular.module('MyApp', []);
 
 app.controller('MyController', ['$http', function($http){
     const controller = this;
+    this.indexOfEditFormToShow;
     this.createTodo = function(){
         $http({
             method:'POST',
@@ -21,6 +22,45 @@ app.controller('MyController', ['$http', function($http){
         }).then(function(response){
             controller.todos = response.data;
             // console.log(controller);
+        })
+    }
+    this.toggleTodoComplete = function(todo){
+        let newCompleteValue;
+        if(todo.complete === true){
+            newCompleteValue = false;
+        } else {
+            newCompleteValue = true
+        }
+
+        $http({
+            method:'PUT',
+            url: '/todos/' + todo._id,
+            data: {
+                description: todo.description,
+                complete: newCompleteValue
+            }
+        }).then(function(response){
+            controller.getTodos();
+        })
+    }
+    this.deleteTodo = function(todo){
+        $http({
+            method:'DELETE',
+            url: '/todos/' + todo._id
+        }).then(function(response){
+            controller.getTodos();
+        })
+    }
+    this.editTodo = function(todo){
+        $http({
+            method:'PUT',
+            url:'/todos/' + todo._id,
+            data: {
+                description: this.updatedDescription,
+                complete:todo.complete
+            }
+        }).then(function(response){
+            controller.getTodos();
         })
     }
     this.getTodos();
