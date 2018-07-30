d55547689412ddf211b0a12cef0488a04b04dc3a
Karolin Rafalski <kr2174@caa.columbia.edu>
2018-07-30 17:03:55 -0400

    karolin code along

--- unit_4/w10d01/instructor_examples/contacts/app/models/location.rb
+++ unit_4/w10d01/instructor_examples/contacts/app/models/location.rb
@@ -1,48 +1,75 @@
 class Location
-    attr_reader :id, :street, :city, :state
-    DB = PG.connect(host: "localhost", port: 5432, dbname: 'contacts')
+  DB = PG.connect(
+    {
+      :host => "localhost",
+      :port => 5432,
+      :dbname => 'contacts_development'
+    }
+  )
 
-    def initialize(opts={})
-        @id = opts["id"].to_i
-        @street = opts["street"]
-        @city = opts["city"]
-        @state = opts["state"]
+  def self.all
+    results = DB.exec("SELECT * FROM locations;")
+    return results.map do |result|
+      {
+        "id" => result["id"].to_i,
+        "street" => result["street"],
+        "city" => result["city"],
+        "state" => result["state"],
+      }
     end
+  end
 
-    def self.all
-        results = DB.exec("SELECT * FROM locations")
-        return results.map { |result| Location.new(result)}
-    end
-    def self.find(id)
-        results = DB.exec("SELECT * FROM locations WHERE id = #{id}")
-        return Location.new(results.first)
-    end
-    def self.create(opts={})
-        results = DB.exec(
-            <<-SQL
-                INSERT INTO locations (street, city, state)
-                VALUES ('#{opts["street"]}', '#{opts["city"]}', '#{opts["state"]}')
-                RETURNING id, street, city, state;
-            SQL
-        )
-        return Location.new(results.first)
-    end
-    def self.delete(id)
-        results = DB.exec("DELETE FROM locations WHERE id=#{id}")
-        return { deleted: true}
-    end
-    def self.update(id, opts={})
-        results = DB.exec(
-            <<-SQL
-                UPDATE locations
-                SET
-                    street='#{opts["street"]}',
-                    city='#{opts["city"]}',
-                    state='#{opts["state"]}'
-                WHERE id = #{id}
-                RETURNING id, street, city, state
-            SQL
-        )
-        return Location.new(results.first)
-    end
+  def self.find(id)
+    # different than notes
+    # .first here! Save typing!
+    results = DB.exec("SELECT * FROM locations WHERE id=#{id}").first
+    return {
+      "id" => results["id"].to_i,
+      "street" => results["street"],
+      "city" => results["city"],
+      "state" => results["state"],
+    }
+
+  end
+
+  def self.create(opts)
+    results = DB.exec(
+      <<-SQL
+        INSERT INTO locations (street, city, state)
+        VALUES ( '#{opts["street"]}', '#{opts["city"]}', '#{opts["state"]}')
+        RETURNING id, street, city, state;
+      SQL
+    ).first
+    return {
+      "id" => results["id"].to_i,
+      "street" => results["street"],
+      "city" => results["city"],
+      "state" => results["state"],
+    }
+
+  end
+
+  def self.delete(id)
+    results = DB.exec("DELETE FROM locations WHERE id=#{id};")
+    return {"deltoid" => true}
+  end
+
+  def self.update(id, opts)
+    results = DB.exec(
+      <<-SQL
+        UPDATE locations
+        SET street='#{opts["street"]}', city='#{opts["city"]}', state='#{opts["state"]}'
+        WHERE id=#{id}
+        RETURNING id, street, city, state;
+      SQL
+    ).first
+    return {
+      "id" => results["id"].to_i,
+      "street" => results["street"],
+      "city" => results["city"],
+      "state" => results["state"],
+    }
+
+  end
+
 end
